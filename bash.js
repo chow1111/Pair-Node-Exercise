85aa39ec9e913f7343a03a33185a028e671f73a0
Jean Chow <jeanchow@jeans-air.lan>
2021-09-24 12:59:52 -0700

    bash.js

--- bash.js
+++ bash.js
@@ -1,11 +1,14 @@
-//output prompt
+const pwd = require('./pwd');
+const ls = require('./ls');
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
-    process.stdout.write('\nprompt > ');
+  if(cmd === 'pwd') {
+      pwd();
+  } else if(cmd === 'fs') {
+    ls();
   } else {
   process.stdout.write('You typed: ' +cmd);
   process.stdout.write('\nprompt > ');
